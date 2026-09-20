1351dcc17218ea43b8eb6d5e40780dd6445b0373
Umesh <umesh.sharmaphurailatpam@bitbasegroup.com>
2026-09-20 18:14:44 +0530

    AuthFinished

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,35 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { ApplicationConfig, isDevMode, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { AuthEffects } from './auth/effects/auth.effects';
+import { authReducer } from './auth/reducer';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     // provideHttpClient()
+    provideStore({
+      'Auth' :  authReducer
+    }),
+    provideEffects([AuthEffects]),
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: !isDevMode(),
+      // actionSanitizer: (action) => {
+      //   if (action.type === '[Auth] Login') {
+      //     return { ...action, password: '***REDACTED***' };
+      //   }
+      //   return action;
+      // },
+      // stateSanitizer: (state) => {
+      //   // also sanitize if it ever lands in state, as a safety net
+      //   return state;
+      // }
+    }),
   ]
 };
